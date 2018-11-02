5bf365d18e54517b30a0f915c27b10a290089819
Alex Jiang <alex.tjiang@gmail.com>
2018-11-02 19:27:00 +0800

    refresh

---
+++ sync_mrrscan.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+sync_files="nh_scan.list mrr_scan.list"
+
+Usage()
+{
+	echo "Usage: $0 {upload|download|help} {pathofsync}"
+}
+
+do_upload () {	
+	echo "==> Upload $sync_files to remote on github.io"
+	git remote -v show
+	git add "$sync_files"
+	git commit -m "refresh"
+	git push -u origin master
+
+}
+
+do_download () {
+	echo "==> Download $sync_files from remote on github.io" 
+}
+
+if [ -z $1 ]; then
+	Usage $0
+	exit 3	
+else
+
+	case "$1" in
+	  upload)
+		METHOD=do_upload		
+		;;
+	  download)
+		METHOD=do_download
+		;;
+	  *)
+		Usage $0
+		exit 3
+		;;
+	esac			
+fi
+
+if [ -z $2 ]; then
+	Usage $0
+	exit 3	
+else
+	GITPATH=$2
+fi
+
+
+while true
+do
+
+	cd "$GITPATH"
+	$METHOD	
+	
+	sleep 10
+done
